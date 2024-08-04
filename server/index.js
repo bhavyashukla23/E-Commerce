4685605b7b7850286b95758912e07874719d1dfd
unknown <bhavyashukla23@gmail.com>
2024-08-04 22:23:01 +0530

    added db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import Connection from './database/db.js';
 
 const app = express();
 const PORT = 8000;
+
+
+Connection();
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
