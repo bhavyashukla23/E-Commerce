0f1c13ebf37253058330641d18c82b422546e1cf
unknown <bhavyashukla23@gmail.com>
2024-08-06 23:33:24 +0530

    db

--- server/database/db.js
+++ server/database/db.js
@@ -1,22 +1,9 @@
-// import mongoose from "mongoose";
-
-// export const Connection=async()=>{
-//     try{
-//         const URL="mongodb+srv://bhavyashukla23:<Mongodb@123>@e-commerce.kos9f1a.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce";
-//         await mongoose.connect(URL);
-//         console.log("Database connected successfully");
-//     }catch(error){
-//         console.log("Error while connecting with db..."+error.message);
-//     }
-// };
-
-// export default Connection;
-
 import mongoose from "mongoose";
 
-export const Connection = async () => {
+export const Connection = async (username,password) => {
     try {
-        const URL = "mongodb+srv://bhavya:Mongodb%40123@e-commerce.kos9f1a.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce";
+        // const URL = "mongodb+srv://bhavya:Mongodb%40123@e-commerce.kos9f1a.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce";
+        const URL = `mongodb+srv://${username}:${password}@e-commerce.kos9f1a.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce`;
         await mongoose.connect(URL, {});
         console.log("Database connected successfully");
     } catch (error) {
