c202c52e62bf6877b431c4d01dd46ac26cb29ae7
unknown <bhavyashukla23@gmail.com>
2024-08-13 16:23:16 +0530

    signup

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const URL ="http://localhost:8000"
+const URL ="http://localhost:8000";
 
-export const authenticateSignup =async(data)=>{
+export const authenticateSignup =async(user)=>{
     try{
-        return await axios.post(`${URL}/signup`,data);
+        return await axios.post(`${URL}/signup`,user);
     }catch(error){
         console.log("Error from api",error.message);
     }
--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,10 +1,18 @@
+import User from '../model/user-schema.js';
 
-
-export const userSignup =(req ,res) =>{
+export const userSignup = async(req ,res) =>{
     try{
-         console.log(req.body);
-    }catch(error){
+         const existingUser = await User.findOne({username : req.body.username});
+         if(existingUser){
+            return res.status(401).json({message: 'User already exists'});
+         }
+        const user= req.body;
+        const newUser=User(user);
+        await newUser.save();
 
+        res.status(200).json({message:user});
+    }catch(error){
+       res.status(500).json({message: error.message});
     }
 
 };
